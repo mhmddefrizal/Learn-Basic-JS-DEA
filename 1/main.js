4a7c7f7fc69b6f256ace2b62aee29225da18b7e3
mhmddefrizal <muhammaddefrizal08@gmail.com>
2024-03-27 03:15:21 +0700

    first commit

---
+++ 1/main.js
@@ -0,0 +1,22 @@
+// alert("hey");
+// prompt("Masukkan usia anda");
+// var usia = 18;
+// const x = 1;
+// let y = 2;
+
+// let usia = 18;
+// usia = 19;
+// console.log("halo mr, usia kamu adalah " + usia);
+// alert("usia anda adalah " + usia);
+
+// let usia = prompt("berapa usia anda");
+// alert("usia anda adalah " + usia);
+
+let nama = "defrizal"; //String
+let usia = 18; //Integer/number
+let tinggiBadan = 178.5; //Tipe double atau float
+let beratBadan;
+
+beratBadan = 70;
+
+alert(`nama saya ${nama} usia saya itu ${usia} tinggi badan saya adalah ${tinggiBadan}cm berat badan saya ${beratBadan}kg`);
