6ff406029d27bf07ab719933c98c15aba62ed5c2
Muhammad Defrizal <muhammaddefrizal08@gmail.com>
2024-03-28 04:16:08 +0700

    second commit

--- 1/main.js
+++ 1/main.js
@@ -1,3 +1,5 @@
+// Beta Code
+
 // alert("hey");
 // prompt("Masukkan usia anda");
 // var usia = 18;
@@ -12,11 +14,38 @@
 // let usia = prompt("berapa usia anda");
 // alert("usia anda adalah " + usia);
 
+// switch (hobi) {
+//   case 1:
+//     hobi = "lorem ipsum";
+//     break;
+
+//   case 2:
+//     hobi = "lorem ipsum dolor sit atmet";
+//     break;
+
+//   default:
+//     hobi = "nothing";
+//     break;
+// }
+
 let nama = "defrizal"; //String
 let usia = 18; //Integer/number
 let tinggiBadan = 178.5; //Tipe double atau float
 let beratBadan;
+let hobi = 1;
 
 beratBadan = 70;
 
-alert(`nama saya ${nama} usia saya itu ${usia} tinggi badan saya adalah ${tinggiBadan}cm berat badan saya ${beratBadan}kg`);
+if (hobi == null) {
+  hobi = "nothing";
+} else {
+  hobi = "have hobby";
+}
+
+let saldoAwal = 50000;
+let saldoTambahan = 80000;
+const saldoAkhir = saldoAwal + saldoTambahan;
+
+alert(`nama saya ${nama} usia saya itu ${usia} tinggi badan saya adalah ${tinggiBadan}cm berat badan saya ${beratBadan}kg dan hobi saya adalah ${hobi}`);
+
+alert(`saldo awal saya sebesar Rp.${saldoAwal} & dan saldo tambahan Rp.${saldoTambahan} jadi total saldo saya berjumlah Rp.${saldoAkhir}`);
